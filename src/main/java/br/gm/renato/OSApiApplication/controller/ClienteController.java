a44ac48acfe1f3b7c8c62a25ad62832d7d0581f5
Renato <renatosandri872@gmail.com>
2026-03-30 10:05:20 -0300

    Término da Parte 2 -Add dependências JPA e MySQL -Configura application.properties com acesso ao BD e show-sql -Mapeia entidade Cliente (@Entity) -Atualiza ClienteController com EntityManager e JPQL

--- src/main/java/br/gm/renato/OSApiApplication/controller/ClienteController.java
+++ src/main/java/br/gm/renato/OSApiApplication/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package br.gm.renato.OSApiApplication.controller;
 
 import br.gm.renato.OSApiApplication.domain.model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author digma
  */
-@RestController 
+@RestController
 public class ClienteController {
-    
-    List<Cliente> listaClientes;
-    
+
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/clientes")
     public List<Cliente> listas() {
-        List<Cliente> listaClientes = new ArrayList<>();
-
-        listaClientes.add(new Cliente(1, "KGe", "kge@teste.com", "11-99999-9999"));
-        listaClientes.add(new Cliente(2, "Maria", "maria@teste.com", "11-88888-8888"));
-        listaClientes.add(new Cliente(3, "Joao", "joao@teste.com", "11-77777-7777"));
-
-        return listaClientes;
+        return manager
+                .createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
